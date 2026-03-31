7362402dcf1955d58a82cd94a689b5377f065ec4
StandBase <vladislav650v@gmail.com>
2026-03-31 20:51:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-print("привет tell")
+TOKEN = "8680410776:AAHg_WutRFYcUzrXCZ6mNuZnYoFlfbJn1yo"
+
+async def dwd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("привет")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("dwd", dwd))
+
+print("Бот запущен...")
+app.run_polling()
